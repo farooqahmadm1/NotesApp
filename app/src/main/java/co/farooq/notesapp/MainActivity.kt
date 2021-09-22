0b8d304d274f7f07037f6477e34b7c4a4f9a1ea1
farooqeddress <farooq@eddress.co>
2021-09-22 19:26:36 +0500

    Initial commit

---
+++ app/src/main/java/co/farooq/notesapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package co.farooq.notesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import co.farooq.notesapp.ui.theme.NotesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesAppTheme {
+
+            }
+        }
+    }
+}
